fa25d293019218a7ea64335706eca95da8252950
SMikulins <smikulins@yahoo.co.uk>
2026-07-04 19:14:33 +0100

    style StatusBadge

--- frontend/src/components/StatusBadge/StatusBadge.jsx
+++ frontend/src/components/StatusBadge/StatusBadge.jsx
@@ -1,9 +1,11 @@
+import "./StatusBadge.css";
+
 function StatusBadge({ status }) {
-    return (
-      <span className={`status-badge status-badge--${status}`}>
-        {status}
-      </span>
-    );
-  }
-  
-  export default StatusBadge;
+  return (
+    <span className={`status-badge status-badge--${status}`}>
+      {status}
+    </span>
+  );
+}
+
+export default StatusBadge;
