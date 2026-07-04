13d37bff7b8fbf2c285aedf12af14e0449eb5153
SMikulins <smikulins@yahoo.co.uk>
2026-07-04 22:18:00 +0100

    add mobile responsiveness

--- frontend/src/components/MatchHistory/MatchHistory.jsx
+++ frontend/src/components/MatchHistory/MatchHistory.jsx
@@ -2,18 +2,19 @@ import "./MatchHistory.css";
 
 function MatchHistory({ sessions, onSelect }) {
     return (
-      <section>
+      <section className="flex flex-col ZZgap-4">
         <h2>Match History</h2>
-  
-        <ul className="match-list">
-          {sessions.map((session) => (
-            <li key={session.id}>
-              <button onClick={() => onSelect(session.id)}>
-                Session #{session.id} — {session.status}
-              </button>
-            </li>
-          ))}
-        </ul>
+        <div className="relative after:sm:hidden after:pointer-events-none after:content-[''] after:block after:h-10 after:w-full after:absolute after:bottom-0 after:left-0 after:bg-linear-to-t after:from-white after:to-transparent">
+          <ul className="match-list">
+            {sessions.map((session) => (
+              <li key={session.id}>
+                <button onClick={() => onSelect(session.id)}>
+                  Session #{session.id} — {session.status}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
     );
   }
